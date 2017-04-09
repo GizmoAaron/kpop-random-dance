6c11bc0645eda21a55c2d5b27392c4d42af02f4b
Lizzie Li <etli2013@gmail.com>
2017-04-09 15:07:54 -0700

    mirror dp videos if not already mirrored

--- js/scripts.js
+++ js/scripts.js
@@ -47,22 +47,6 @@ function cleanseData(data) {
   return cleanData;
 };
 
-// Durstenfeld shuffle
-function shuffle(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-};
-
-// convert timestamp to seconds
-function toSeconds(timestamp) {
-  return parseInt(timestamp.split(':')[0]) * 60 + parseInt(timestamp.split(':')[1]);
-};
-
 // do stuff
 $(document).on('ajaxComplete', function() {
   playlist = shuffle(masterList);
@@ -124,7 +108,30 @@ function loadVideo(videoObj) {
       'suggestedQuality': 'large'
     });
   }
+  
+  // mirror the dance practice video if not already mirrored
+  if (dpOK(videoObj) && videoObj.mirror == 'FALSE')
+    $('#player').addClass('mirror');
+  else
+    $('#player').removeClass('mirror');
+  
   endSecondsTemp = getEndSeconds(videoObj);
+};
+
+// Durstenfeld shuffle
+function shuffle(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+// convert timestamp to seconds
+function toSeconds(timestamp) {
+  return parseInt(timestamp.split(':')[0]) * 60 + parseInt(timestamp.split(':')[1]);
 };
 
 // helper functions
